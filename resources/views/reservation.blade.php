b1842f51eaed8c52a8a9657195c2fab5528dc9b6
Lillian-Asus\Administrator <lillian.yingying.wei@gmail.com>
2024-04-25 18:21:11 -0400

    first commit

---
+++ resources/views/reservation.blade.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <form action="{{ route('submitReservation') }}" method="POST" id="reservationForm">
+        @csrf
+        <div class="form-outline mb-4">
+            <input type="text" id="name" name="name" class="form-control" required />
+            <label class="form-label" for="name">Name</label>
+        </div>
+
+        <div class="form-outline mb-4">
+            <input type="email" id="email" name="email" class="form-control" required />
+            <label class="form-label" for="email">Email address</label>
+        </div>
+
+        <div class="form-outline mb-4">
+            <input type="tel" id="phone" name="phone" class="form-control" required />
+            <label class="form-label" for="phone">Phone number</label>
+        </div>
+
+        <div class="form-outline mb-4">
+            <input type="date" id="checkInDate" name="checkInDate" class="form-control" required />
+            <label class="form-label" for="checkInDate">Check-in Date</label>
+        </div>
+
+        <div class="form-outline mb-4">
+            <input type="date" id="checkOutDate" name="checkOutDate" class="form-control" required />
+            <label class="form-label" for="checkOutDate">Check-out Date</label>
+        </div>
+
+        <button type="submit" class="btn btn-primary btn-block">Submit Reservation</button>
+    </form>
+
+
+
+
+</body>
+</html>
