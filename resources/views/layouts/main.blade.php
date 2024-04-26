f6a4e9ac1e2aff06ec469badd890db8a751aae28
aqizzz <aqizzz@u.circlecat.org>
2024-04-26 19:09:04 -0400

    Add icon and favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,6 +14,7 @@
   <link href="{{ asset('resources/css/mdb.min.css') }}" rel="stylesheet">
   <!-- Your custom styles (optional) -->
   <link href="{{ asset('resources/css/style.css') }}" rel="stylesheet">
+  <link rel="icon" href="{{ asset('resources/favicon/favicon.ico') }}" type="image/x-icon">
 </head>
 
 <body>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -20,19 +20,19 @@
       <!-- Navbar brand -->
       <a class="navbar-brand mt-2 mt-lg-0" href="#">
         <img
-          src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-          height="15"
-          alt="MDB Logo"
+          src="{{ asset('resources/images/hotel.png') }}"
+          height="50"
+          alt="Hotel Logo"
           loading="lazy"
         />
       </a>
       <!-- Left links -->
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">Dashboard</a>
+          <a class="nav-link" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Team</a>
+          <a class="nav-link" href="#">Rooms</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Projects</a>
