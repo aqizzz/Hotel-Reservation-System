e9a5cd7ec8fe74b3c052b3f859a5585328fabc12
Lillian-Asus\Administrator <lillian.yingying.wei@gmail.com>
2024-04-29 07:49:32 -0400

    nothing

--- resources/views/roomDetails.blade.php
+++ resources/views/roomDetails.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <h1>Room Details</h1>
+    <!-- <h1>Room Details</h1>
 
     <ul>
         @foreach ($roomDetails as $room)
@@ -14,7 +14,26 @@
                 Rate: {{ $room['rate'] }}
             </li>
         @endforeach
-    </ul>
-@endsection
+    </ul> -->
 
+
+
+
+    <div class="container">
+        <div class="row">
+            @foreach ($roomDetails as $room)
+                <div class="col-md-3 mb-4">
+                    <div class="card">
+                        <img src="/images/{{ strtolower(str_replace(' ', '-', $room['roomtype'])) }}.webp" class="card-img-top" alt="{{ $room['roomtype'] }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $room['roomtype'] }}</h5>
+                            <p class="card-text">Capacity: {{ $room['capacity'] }}</p>
+                            <p class="card-text">Rate: {{ $room['rate'] }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
 
