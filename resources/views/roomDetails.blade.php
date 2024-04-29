b7c571f920043b892dc79f8f73bb7e2c49f587fd
Lillian-Asus\Administrator <lillian.yingying.wei@gmail.com>
2024-04-29 06:37:54 -0400

    roomDetails

--- resources/views/roomDetails.blade.php
+++ resources/views/roomDetails.blade.php
@@ -3,62 +3,18 @@
 @section('title', 'Welcome to XX Hotel')
 
 @section('content')
-    
-    <!-- <h1>Room Details</h1>
+
+    <h1>Room Details</h1>
+
     <ul>
-        @foreach($roomDetails as $room)
-            <li>Room Number: {{ $room['room_number'] }}, Capacity: {{ $room['capacity'] }}, Rate: {{ $room['rate'] }}</li>
+        @foreach ($roomDetails as $room)
+            <li>
+                Room Type: {{ $room['roomtype'] }},
+                Capacity: {{ $room['capacity'] }},
+                Rate: {{ $room['rate'] }}
+            </li>
         @endforeach
-    </ul> -->
-
-
-
-    <h1 class="text-center my-5">Room Details</h1>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="../images/room1.webp" class="card-img-top" alt="Room 1">
-                    <div class="card-body">
-                        <h5 class="card-title">Room 101</h5>
-                        <p class="card-text">Description for Room 1</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="../images/room2.webp" class="card-img-top" alt="Room 2">
-                    <div class="card-body">
-                        <h5 class="card-title">Room 102</h5>
-                        <p class="card-text">Description for Room 2</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="../images/room3.webp" class="card-img-top" alt="Room 3">
-                    <div class="card-body">
-                        <h5 class="card-title">Room 103</h5>
-                        <p class="card-text">Description for Room 3</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card">
-                    <img src="../images/room4.webp" class="card-img-top" alt="Room 4">
-                    <div class="card-body">
-                        <h5 class="card-title">Room 104</h5>
-                        <p class="card-text">Description for Room 4</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-   
-
-
+    </ul>
 @endsection
 
 
